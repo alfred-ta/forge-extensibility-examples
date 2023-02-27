038cce491efbbc4cb18e5d7bf46f8d8405ef5fd1
Alfred Takala <alfredtakala@yandex.com>
2023-02-27 18:39:09 +0800

    install app when there is no existing InstalledApps instance for the site

--- cloudCode.js
+++ cloudCode.js
@@ -64,44 +64,52 @@ const installApp = async({ appId, siteId, userId }) => {
     if (siteId) query.equalTo('SiteId', siteId.toString());
     if (userId) query.equalTo('UserId', userId.toString());
     query.include('InstanceList');
-    // query.find('InstanceList.AppsList');
     const record = await query.first();
+    const appInstance = await createAppInstance(siteNameId, appId, 'site');
     if (record) {
-    // if (record && record.get('InstanceList') && record.get('InstanceList').length > 0) {
-      
       let instanceList = record.get('InstanceList') || [];
-      // let appInstance = instanceList.find(obj => {
-      //   const appsList = obj.get('Developer_App');
-      //   console.log('====================', obj);
-      //   if (appsList) {
-      //     const appsIds = appsList.map(app => app.objectId);
-      //     console.log('apps ids', appsIds);
-      //     return (appsIds.includes(appId))
-      //   }
-      //   return false;
-      // });
-      // if (!appInstance) 
-      const appInstance = await createAppInstance(siteNameId, appId, 'site');
       instanceList.push(appInstance);
       record.set('InstanceList', instanceList);
       await record.save();
       return record.id;
-    // } else {
-    //   const APP_INSTANCE_MODEL_NAME = `ct____${siteNameId}____App_Instance`;
-    //   const appInstanceQuery = new Parse.Query(APP_INSTANCE_MODEL_NAME);
-    //   appInstanceQuery.equalTo('objectId', instanceId);
-
-    //   const appInstance = await appInstanceQuery.first();
-    //   const appsList = appInstance.get('AppsList');
-    //   appsList.push(developerApp);
-    //   appInstance.set('InstanceList', instanceList);
-    //   await appInstance.save();
-    //   return appInstance.id;
+    } else {
+      const newInstalledApps = createInstalledApps(siteNameId, siteId, userId, appInstance);
+      return newInstalledApps.id;
     }
 
   } catch(error) {
     console.error('inside installApp function', error);
     throw error;
+  }
+}
+
+const createInstalledApps = async(siteNameId, siteId, userId, appInstance) => {
+  try {
+    const INSTALLED_APPS_MODEL_NAME = `ct____${siteNameId}____InstalledApps`;
+    const InstalledAppsModel = Parse.Object.extend(INSTALLED_APPS_MODEL_NAME);
+    const query = new Parse.Query(INSTALLED_APPS_MODEL_NAME);
+    const sample = await query.first();
+    let slug = '';
+
+    const installedApps = new InstalledAppsModel();
+    installedApps.set('InstanceList', [appInstance]);
+    installedApps.set('t__status', 'Published');
+    if (siteId) {
+      installedApps.set('SiteId', siteId.toString());
+      slug = `${siteId}-${appInstance.get('Slug')}`
+    }
+    if (userId) {
+      installedApps.set('UserId', userId.toString());
+      slug = `${userId}-${appInstance.get('Slug')}`
+    }
+    installedApps.set('Slug', slug);
+    installedApps.set('t__model', sample.get('t__model'));
+    installedApps.set('ACL', sample.get('ACL'));
+    installedApps.set('t__color', sample.get('t__color'));
+    await installedApps.save();
+    return installedApps;
+  } catch(error) {
+    console.error('inside createInstalledApps function', error);
   }
 }
 
@@ -118,13 +126,11 @@ const createAppInstance = async (siteNameId, developerAppId, kind = 'site') => {
 
     const sampleAppInstanceQuery = new Parse.Query(APP_INSTANCE_MODEL_NAME);
     const sampleAppInstance = await sampleAppInstanceQuery.first();
-
-    console.log('*******************', sampleAppInstance)
-
+    const slug = `${kind}-${developerApp.get('Slug')}_instance`;
     const appInstance = new AppInstanceModel();
     appInstance.set('Developer_App', [developerApp]);
     appInstance.set('t__status', 'Published');
-    appInstance.set('Slug', `${kind}-${developerApp.get('Slug')}_instance`);
+    appInstance.set('Slug', slug);
     appInstance.set('t__model', sampleAppInstance.get('t__model'));
     appInstance.set('ACL', sampleAppInstance.get('ACL'));
     appInstance.set('t__color', sampleAppInstance.get('t__color'));
